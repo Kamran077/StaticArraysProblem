dd701a2570f357b4fa66028edffc934d1727b7b8
Kamran <157282987+Kamran077@users.noreply.github.com>
2024-06-13 12:58:52 +0400

    Add files via upload

---
+++ Arrays-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[20];
+            Random rm = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                nums[i] = rm.Next(1, 20);
+                if (nums[i] % 2 == 0)
+                {
+                    Console.Write(nums[i] + " ");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
