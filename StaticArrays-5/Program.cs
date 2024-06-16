cba6515e55994bbad0e848975a4da90172e4ecf2
Kamran <157282987+Kamran077@users.noreply.github.com>
2024-06-16 08:17:24 +0400

    Add files via upload

---
+++ StaticArrays-5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticArrays_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            double oddsum = 0;
+            int evensum = 0;
+            double average = 0;
+            int count = 0;
+            Random rm = new Random();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rm.Next(1, 20);
+
+                if (nums[i] % 2 == 0)
+                {
+                    evensum += nums[i];
+                }
+                else
+                {
+                    oddsum += nums[i];
+                    count++;
+                }
+            }
+            average = oddsum / count;
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i + " ");
+            }
+            Console.WriteLine("*****************");
+
+            Console.WriteLine(evensum);
+            Console.WriteLine(average);
+
+
+            Console.ReadLine();
+        }
+    }
+}
