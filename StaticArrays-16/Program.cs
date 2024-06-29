ed0feed37bd0f5fafc546364162903bfa4a30d9e
Kamran <157282987+Kamran077@users.noreply.github.com>
2024-06-29 08:31:23 +0400

    Add files via upload

---
+++ StaticArrays-16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticArrays_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ölçüsü 10 olan massiv yaradın və içərsini random rəqəmlərlə
+            //doldurun.Massivdəki rəqəmlərin yerlərini tərsinə çevirən program
+            //yazın. (0 - cı indeksi 9 - la, 1 - i 8 - lə və s.)
+            int[] nums = new int[10];
+            Random rm = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rm.Next(0, 101);
+            }
+            Console.WriteLine("Array:");
+            foreach (int numbers in nums)
+            {
+                Console.Write(numbers + " ");
+            }
+            Console.WriteLine("\nArray tersi: ");
+            for (int j = nums.Length - 1; j >= 0; j--)
+            {
+                Console.Write(nums[j] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
